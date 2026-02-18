95539e4340808b60458d6dfc65c825c193ebca21
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-02-18 14:49:59 +0530

    Update BaseClass.java

--- CucumberProject1/src/test/java/org/base/BaseClass.java
+++ CucumberProject1/src/test/java/org/base/BaseClass.java
@@ -4,11 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-//import java.nio.file.FileAlreadyExistsException;
-//import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DateUtil;
@@ -182,32 +179,4 @@ public class BaseClass {
 		FileOutputStream fos=new FileOutputStream(f);
 		wb.write(fos);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
