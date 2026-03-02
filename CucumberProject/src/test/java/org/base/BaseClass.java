b4f948e824b10c20e59184d1a54077b89f256380
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-03-02 15:47:32 +0530

    Update BaseClass.java

--- CucumberProject/src/test/java/org/base/BaseClass.java
+++ CucumberProject/src/test/java/org/base/BaseClass.java
@@ -6,7 +6,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DateUtil;
@@ -181,4 +180,5 @@ public class BaseClass {
 		wb.write(fos);
 	}
 }
+
 
